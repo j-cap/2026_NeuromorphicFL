ceb9529dd9c684225dd72ac0d421e195de24715a
Jakob <jakobweber@hotmail.com>
2026-08-29 09:58:19 +0200

    Add NeuromorphicFL package init

---
+++ src/neuromorphicfl/__init__.py
@@ -0,0 +1,15 @@
+"""Reusable primitives for NeuromorphicFL experiments."""
+
+from .objectives import QuadraticObjective, PiecewiseQuadraticObjective
+from .optimizers import SGD, SignSGD, IFSGD, LIFSGD
+from .simulation import run_scalar_optimizer
+
+__all__ = [
+    "QuadraticObjective",
+    "PiecewiseQuadraticObjective",
+    "SGD",
+    "SignSGD",
+    "IFSGD",
+    "LIFSGD",
+    "run_scalar_optimizer",
+]
